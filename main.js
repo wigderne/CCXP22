6acd02ac1fbf5a9db8d01e48cf2121ee9e5a2bd1
Wigder <wigderne@gmail.com>
2023-11-23 18:29:21 -0300

    Versão com alteração

---
+++ main.js
@@ -0,0 +1,58 @@
+function addRedBorder (id) 
+{
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid red";
+}
+
+//addRedBorder("quinta");
+
+function highlightCard(selector)
+{
+    var element = document.querySelector(selector);
+    element.classList.toggle("card-highlight")
+}
+
+function addKeyboardEventListeners()
+{
+
+    document.addEventListener('keydown', (event) => {
+
+        var ingresso1 = document.getElementById("quinta");
+        var ingresso2 = document.getElementById("sexta");
+        var ingresso3 = document.getElementById("sabado");
+        var ingresso4 = document.getElementById("domingo");
+
+        var code = event.code;
+        if(code == 'Digit1') {
+            ingresso1.classList.toggle("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code == 'Digit2') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.toggle("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code == 'Digit3') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.toggle("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code == 'Digit4') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.toggle("card-highlight");
+        }
+
+    }, false);
+
+}
+
+addKeyboardEventListeners();
